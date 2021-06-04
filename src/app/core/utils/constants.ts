29daf5352521a881e4f4ad9534306baf43d6270f
binaryprotagonist@gmail.com <binaryprotagonist@example.com>
2021-06-04 20:16:46 +0530

    http to https

--- src/app/core/utils/constants.ts
+++ src/app/core/utils/constants.ts
@@ -2,7 +2,7 @@ export const constants = {
     // apiBaseURL: 'https://mvpnotes-backend.herokuapp.com/',
     //  apiBaseURL:'http://localhost:3000/',
     //angularBaseURL:'http://34.219.216.136:9092/',
-     apiBaseURL:   'http://backendformvp.herokuapp.com/',
+     apiBaseURL:   'https://backendformvp.herokuapp.com/',
     headers: { 'accept': 'application/json', 'content-type': 'application/json' },
 };
 
