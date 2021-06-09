1ea86d59bd3e55fe65af510b352f2fa7cfcbecef
Binary Protagonist <63355451+binaryprotagonist@users.noreply.github.com>
2021-06-09 15:50:57 +0530

    Profile section fix and Email Pop UI fix

--- src/app/pages/my-account/my-account.component.ts
+++ src/app/pages/my-account/my-account.component.ts
@@ -17,111 +17,50 @@ import { ImageCroppedEvent } from 'ngx-image-cropper';
   styleUrls: ['./my-account.component.css']
 })
 export class MyAccountComponent implements OnInit {
-  socialLogin:any;
+  socialLogin: any;
   modalRef: BsModalRef;
   updateForm: FormGroup;
-  emailUpdateForm:FormGroup;
-  passwordForm:FormGroup;
-  submitted:boolean=false
-  submitted1:boolean=false
-  submitted2:boolean=false
+  emailUpdateForm: FormGroup;
+  passwordForm: FormGroup;
+  submitted = false;
+  submitted1 = false;
+  submitted2 = false;
 
-  firstName:any
-  lastName:any
-  email:any;
-  gender:any;
-  edit:boolean=false
-  editEmail:boolean=false
-  error:any
-  profile:any
-  profileImage:any
-  PlanName:any
+  firstName: any;
+  lastName: any;
+  email: any;
+  gender: any;
+  edit = false;
+  editEmail = false;
+  error: any;
+  profile: any;
+  profileImage: any;
+  PlanName: any;
   imageChangedEvent: any = '';
   croppedImage: any = '';
-  oldEmail:any
+  oldEmail: any;
+  userData;
   @ViewChild('documentEditForm') documentEditForm: FormGroupDirective;
 
-  constructor(private _router: Router,private commonService: CommonService,private formBuilder:FormBuilder,private modalService: BsModalService,private toastr: ToastrService, private spinner: NgxSpinnerService) { }
+  constructor (private _router: Router,private commonService: CommonService,private formBuilder:FormBuilder,private modalService: BsModalService,private toastr: ToastrService, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.getUser();
-  //   this.updateForm = this.formBuilder.group({
-  //     firstName: [{value: '', disabled: true}, [
-  //       Validators.required,
-  //   ]
-  // ],
-  // lastName: [{value: '', disabled: true}, [
-  //   Validators.required,
 
-  //   ]
-  // ],
-  // email: [{value: '', disabled: true},, [
-  //   Validators.required,
-  //   Validators.email,
-  //   Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")
-
-  //   ]
-  // ],
-
-
-  //  });
-  this.updateForm = this.formBuilder.group({
-    firstName: [{value: '', disabled: true}, [
-      Validators.required,
-  ]
-],
-lastName: [{value: '', disabled: true}, [
-  Validators.required,    ]
-],
-// email: [{value: '', disabled: true}, [
-//   Validators.required,
-//   Validators.email,
-//   Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")    ]
-// ]
-});
-this.emailUpdateForm=this.formBuilder.group({
-  email: [{value: '', disabled: true}, [
-      Validators.required,
-      Validators.email,
-      // Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")
-       ]
-    ]
-})
-   this.passwordForm = this.formBuilder.group({
-      password:['',
-      [
-      Validators.required,
-      Validators.pattern(/^\S*$/),
-      Validators.minLength(6),
-      Validators.maxLength(12),
-
-      ]
-    ],
-    newPassword:['',
-      [
-      Validators.required,
-      Validators.pattern(/^\S*$/),
-      Validators.minLength(6),
-      Validators.maxLength(12),
-
-      ]
-    ],
-    confirmPassword:['',
-      [
-      Validators.required,
-      Validators.pattern(/^\S*$/),
-      Validators.minLength(6),
-      Validators.maxLength(12),
-
-      ]
-    ],
-
-   }
-   ,{
-    validator: this.ConfirmedValidator('newPassword', 'confirmPassword')
-   })
-
+    this.updateForm = this.formBuilder.group({
+      firstName: [{value: '', disabled: true}, [Validators.required, ]],
+      lastName: [{value: '', disabled: true}, [Validators.required, ]],
+    });
+    this.emailUpdateForm = this.formBuilder.group({
+      email: [{value: '', disabled: true}, [Validators.required, Validators.email, ]]
+    });
+    this.passwordForm = this.formBuilder.group({
+     password: ['', [ Validators.required, Validators.pattern(/^\S*$/), Validators.minLength(6), Validators.maxLength(12), ]],
+     newPassword: ['', [ Validators.required, Validators.pattern(/^\S*$/), Validators.minLength(6), Validators.maxLength(12), ]],
+     confirmPassword: ['', [ Validators.required, Validators.pattern(/^\S*$/), Validators.minLength(6), Validators.maxLength(12), ]],
+   }, { validator: this.ConfirmedValidator('newPassword', 'confirmPassword') });
   }
+
   ngAfterViewInit() {
     $('#OpenImgUpload').click(function(){ $('#imgupload').trigger('click'); });
   }
@@ -135,193 +74,180 @@ this.emailUpdateForm=this.formBuilder.group({
 
   getUser(){
     this.spinner.show();
-    this.commonService.get(`getUser`).subscribe((data: any)=>{
-      if(data.status==200){
-         console.log("datda",data.result)
-        this.firstName=data.result.firstName;
-        this.lastName=data.result.lastName;
-        this.email=data.result.email;
-        this.oldEmail = data.result.email
-        this.profileImage=data.result.profileImage
-        this.gender=data.result.gender?data.result.gender:''
-        this.socialLogin=data.result.socialLogin
-        this.PlanName =data.result.PlanName
+    this.commonService.get(`getUser`).subscribe((data: any) => {
+      if (data.status == 200){
+        this.userData = data.result;
+        this.firstName = data.result.firstName;
+        this.lastName = data.result.lastName;
+        this.email = data.result.email;
+        this.oldEmail = data.result.email;
+        this.profileImage = data.result.profileImage;
+        this.gender = data.result.gender ? data.result.gender : '';
+        this.socialLogin = data.result.socialLogin;
+        this.PlanName = data.result.PlanName;
         this.spinner.hide();
-        console.log(this.profileImage)
-
+        console.log(this.profileImage);
       }
-  })
+  });
 }
-editProfile(value){
-  this.edit=value
-  console.log("value",value)
-  this.updateForm.enable();
-
-}
-updateProfile(){
-  this.submitted = true;
-  if(this.updateForm.invalid){
-    return ;
-  }
-  if(this.gender=="" || this.gender==undefined ){
-    this.error="gnder selecction is required"
-    return ;
-  }
-  this.spinner.show();
-  let body={
-    firstName:this.firstName,
-    lastName:this.lastName,
-    email:this.email,
-    gender:this.gender
-   }
-   this.commonService.post('editProfile',body).subscribe((data: any)=>{
-    console.log(data)
-    if(data.status==200){
-      this.toastr.success('update Successfully', 'success');
+  editProfile(value) {
+    this.edit = value;
+    this.updateForm.enable();
+    if (value) { this.updateForm.enable(); }
+    else {
+      this.firstName = this.userData.firstName;
+      this.lastName = this.userData.lastName;
+      this.gender = this.userData.gender;
       this.updateForm.disable();
-      this.edit =false
-      this.getUser();
-      this.spinner.hide();
     }
-    if(data.status==204){
-        this.toastr.error('Email already exsist', 'error');
+  }
+
+  updateProfile(){
+    this.submitted = true;
+    if (this.updateForm.invalid){
+      return;
+    }
+    if (this.gender === '' || this.gender === undefined ){
+      this.error = 'Gender selection is required';
+      return;
+    }
+    this.spinner.show();
+    const body = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      gender: this.gender
+    };
+    this.commonService.post('editProfile', body).subscribe((data: any) => {
+      console.log(data)
+      if (data.status === 200){
+        this.toastr.success('Details Updated Successfully', 'success');
         this.updateForm.disable();
-        this.edit =false
+        this.edit = false;
         this.getUser();
         this.spinner.hide();
-    }
-    else{
-      this.spinner.hide();
-
-    }
-})
-
-
+      }
+      if (data.status == 204){
+          this.toastr.error('Email already exist', 'error');
+          this.updateForm.disable();
+          this.edit = false;
+          this.getUser();
+          this.spinner.hide();
+      }
+      else{
+        this.spinner.hide();
+      }
+    });
   }
 
-editMail(value){
-  this.editEmail = value;
-  console.log('value', value);
-  if (value) { this.emailUpdateForm.enable(); }
-  else { this.emailUpdateForm.disable(); }
-}
-updateEmail(){
-  // alert(this.submitted2)
-  this.submitted2 = true;
-  if(this.emailUpdateForm.invalid){
-    return ;
-  }
-  this.spinner.show();
-  let body={
-    email:this.email,
-    emailOld:this.oldEmail
-   }
-   this.commonService.post('emailUpdate',body).subscribe((data: any)=>{
-    console.log(data)
-    if(data.status==200){
-      // this.toastr.success('update Successfully', 'success');
-      Swal.fire('please check your mail!!',
-      '',
-      'success');
+  editMail(value){
+    this.editEmail = value;
+    console.log('value', value);
+    if (value) { this.emailUpdateForm.enable(); }
+    else {
+      this.email = this.userData.email;
       this.emailUpdateForm.disable();
-      this.editEmail =false
-      this.getUser();
-      this.spinner.hide();
+     }
+  }
+  updateEmail(){
+    this.submitted2 = true;
+    if (this.emailUpdateForm.invalid){
+      return ;
     }
-    if(data.status==204){
-        this.toastr.error('Email already exsist', 'error');
+    this.spinner.show();
+    const body = {
+      email: this.email,
+      emailOld: this.oldEmail
+    };
+    this.commonService.post('emailUpdate', body).subscribe((data: any) => {
+      console.log(data);
+      if (data.status == 200){
+        // this.toastr.success('update Successfully', 'success');
+        Swal.fire('Please check your mail!', '', 'success');
         this.emailUpdateForm.disable();
-        this.editEmail =false
+        this.editEmail = false;
         this.getUser();
         this.spinner.hide();
-    }
-    else{
-      this.spinner.hide();
+      }
+      if (data.status == 204){
+          this.toastr.error('Email already exist', 'error');
+          this.emailUpdateForm.disable();
+          this.editEmail = false;
+          this.getUser();
+          this.spinner.hide();
+      } else {
+        this.spinner.hide();
+      }
+    });
+  }
 
-    }
-})
-}
 
-
-  fileChangeEvent(event: any,template): void {
+  fileChangeEvent(event: any, template): void {
     this.imageChangedEvent = event;
     this.openModal(template);
-}
-imageCropped(event: ImageCroppedEvent) {
-  this.croppedImage = event.base64;
-  console.log(this.croppedImage)
-}
-imageLoaded() {
-  // show cropper
-}
-cropperReady() {
-  // cropper ready
-}
-loadImageFailed() {
-  // show message
-}
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+  }
+
+  imageLoaded() {
+  }
+
+  cropperReady() {
+  }
+
+  loadImageFailed() {
+  }
+
   onProfileChange(event){
     const file = event.target.files[0];
     this.spinner.show();
     const formData = new FormData();
     formData.append('file', file);
-    this.commonService.post('uploadImage',formData).subscribe((data: any)=>{
-      if(data.status==200){
+    this.commonService.post('uploadImage', formData).subscribe((data: any) => {
+      this.spinner.hide();
+      if (data.status == 200){
         this.getUser();
-        this.spinner.hide();
-      }else{
       }
-    },
-    (error) => {
-      })
-
+    }, (error) => { this.spinner.hide(); });
   }
 
   uploadProfile() {
-    let body={
-      data:this.croppedImage
-    }
-    this.commonService.post('base64img',body).subscribe((data: any)=>{
-      console.log(data)
+    const body = {
+      data: this.croppedImage
+    };
+    this.commonService.post('base64img', body).subscribe((data: any) => {
+      console.log(data);
       this.getUser();
-      this.modalRef.hide()
-
-    },(error)=>{
-      console.log('this.profile error',error);
-      this.modalRef.hide()
-      // this.closeModal();
-     // this.spinner.hide();
+      this.modalRef.hide();
+    }, (error) => {
+      console.log('this.profile error', error);
+      this.modalRef.hide();
+      this.spinner.hide();
     });
-
-
   }
-onProfileRemove(){
+  onProfileRemove(){
   Swal.fire({
-    title: "Are you sure?",
+    title: 'Are you sure?',
     showConfirmButton: true,
     showCancelButton: true,
     confirmButtonText: 'Yes,Remove it!'
-  })
-    .then((willDelete) => {
-      if (willDelete.value) {
-        this.ProfileRemove()
-      }
-      console.log(willDelete)
-    })
-}
+  }).then((willDelete) => {
+      if (willDelete.value) { this.ProfileRemove(); }
+    });
+  }
 
   ProfileRemove(){
-    let body={
-      profileImage:null
-     }
-     this.commonService.post('removeProfileImage',body).subscribe((data: any)=>{
-      console.log("removeProfileImage",data.data)
-      if(data.status==200){
-        this.toastr.success('remove Successfully', 'success');
-        this.getUser();
-      }else{
-      }
-    })
+    const body = {
+      profileImage: null
+     };
+    this.commonService.post('removeProfileImage', body).subscribe((data: any) => {
+    if (data.status == 200) {
+      this.toastr.success('remove Successfully', 'success');
+      this.getUser();
+    }
+   });
   }
 
   onSubmit(){
@@ -365,7 +291,7 @@ onProfileRemove(){
     this._router.navigate(["login"]);
 
   }
-onDelete(){
+  onDelete(){
   Swal.fire({
     title: "Are you sure?",
     text: "Once deleted, you will not be able to recover this imaginary file!",
@@ -380,8 +306,8 @@ onDelete(){
       }
       console.log(willDelete)
     })
-}
-deleteUsers(){
+  }
+  deleteUsers(){
   let body={
     isStatus:2,
    }
@@ -394,5 +320,5 @@ deleteUsers(){
     }else{
     }
 })
-}
+  }
 }
