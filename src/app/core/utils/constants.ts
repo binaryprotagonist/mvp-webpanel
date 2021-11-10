ebeaeb02af99df29d591a0dcfd4fffa57e60c1c5
binaryprotogonist@gmail.com <binaryprotogonist@gmail.com>
2021-11-10 11:02:34 +0530

    fix (http): change url to live url

--- src/app/core/utils/constants.ts
+++ src/app/core/utils/constants.ts
@@ -1,6 +1,6 @@
 export const constants = {
-    apiBaseURL: 'http://localhost:3000/',
-    // apiBaseURL: 'https://backendformvp.herokuapp.com/',
+    // apiBaseURL: 'http://localhost:3000/',
+    apiBaseURL: 'https://backendformvp.herokuapp.com/',
     headers: { 'accept': 'application/json', 'content-type': 'application/json' },
 };
 
