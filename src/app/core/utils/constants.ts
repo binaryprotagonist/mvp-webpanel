bce2296b8271bcced63bdb56cc24e77c060104c6
binaryprotagonist@gmail.com <binaryprotagonist@example.com>
2021-11-13 11:57:11 +0530

    add invoice mail

--- src/app/core/utils/constants.ts
+++ src/app/core/utils/constants.ts
@@ -1,7 +1,6 @@
 export const constants = {
-    // apiBaseURL: 'http://localhost:3000/',
+     apiBaseURL: 'http://localhost:3100/',
     // apiBaseURL: 'https://backendformvp.herokuapp.com/',
-    apiBaseURL: 'https://stripe-mvp.herokuapp.com/',
+    // apiBaseURL: 'https://stripe-mvp.herokuapp.com/',
     headers: { 'accept': 'application/json', 'content-type': 'application/json' },
 };
-
